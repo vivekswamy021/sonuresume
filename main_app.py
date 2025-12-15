fbfdc710b2a85740c7eba95ee33329737eb99357
VIVEK <vivekswamy021@gmail.com>
2025-12-15 16:03:09 +0530

    Clean up logo URL instructions in main_app.py
    Removed commented section for logo URL instructions.

--- main_app.py
+++ main_app.py
@@ -4,7 +4,6 @@ from admin_dashboard import admin_dashboard
 from candidate_dashboard import candidate_dashboard
 from hiring_dashboard import hiring_dashboard
 
-# --------------------------------------------------
 # 🔥 SET YOUR LOGO HERE (GitHub RAW IMAGE LINK)
 # --------------------------------------------------
 LOGO_URL = "https://raw.githubusercontent.com/vivekswamy021/Pragyan_AI_resume/main/pragyan_ai_school_cover.jpg"
